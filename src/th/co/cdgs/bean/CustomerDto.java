2ddd121ec7bf84fc6accb28511370821c73ade96
Salmon rice <salmon.witch@gmail.com>
2019-11-23 16:23:15 +0700

    1st workshop

---
+++ src/th/co/cdgs/bean/CustomerDto.java
@@ -0,0 +1,56 @@
+package th.co.cdgs.bean;
+
+@SuppressWarnings("unused")
+public class CustomerDto {
+	private Long customerId;
+	private String fullName;
+	private String firstname;
+	private String lastname;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	private String tel;
+	private String address;
+	
+	private String email;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
